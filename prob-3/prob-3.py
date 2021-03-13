9a37c994701c4823178991e629f647bac8318338
Alen Sugimoto <alensugimoto@gmail.com>
2021-03-13 14:14:03 +0100

    correctly solve problem 3

--- prob-3/prob-3.py
+++ prob-3/prob-3.py
@@ -6,12 +6,19 @@ while input_n > 0:
     n = input_n - 1
     s = ''
     while n > 0:
-        pow_2 = 2 ** int(math.log(n) / math.log(2))
-        right = 3 ** int(math.log(pow_2) / math.log(2))
+        # convert 'n' to the greatest power of 2 less than or equal to it
+        bits = bin(n)
+        pow_2 = int(bits[:3] + '0' * (len(bits) - 3), 2)
+        # find the log base 2 of 'pow_2'
+        log_pow_2 = len(bin(pow_2)) - 3
+        # get rightmost element
+        right = 3 ** log_pow_2
+        # store in 's'
         if n == input_n - 1:
             s = str(right) + ' ' + s
         else:
             s = str(right) + ', ' + s
+        # prepare for next iteration
         n -= pow_2
     print(s + '}')
     input_n = int(input())
