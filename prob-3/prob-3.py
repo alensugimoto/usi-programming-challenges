e2743d54a1fd6f162c9d8d87777aa38e92395b11
Alen Sugimoto <alensugimoto@gmail.com>
2021-03-13 13:37:24 +0100

    try 3 of problem 3

--- prob-3/prob-3.py
+++ prob-3/prob-3.py
@@ -1,20 +1,17 @@
 import math
 
-
-def rec(n, is_first):
-    if n != 0:
+input_n = int(input())
+while input_n > 0:
+    print('{ ', end='')
+    n = input_n - 1
+    s = ''
+    while n > 0:
         pow_2 = 2 ** int(math.log(n) / math.log(2))
         right = 3 ** int(math.log(pow_2) / math.log(2))
-        rec(n - pow_2, False)
-        print(right, end='')
-        if not is_first:
-            print(',', end='')
-        print(' ', end='')
-
-
-n = int(input())
-while n > 0:
-    print('{ ', end='')
-    rec(n - 1, True)
-    print('}')
-    n = int(input())
+        if n == input_n - 1:
+            s = str(right) + ' ' + s
+        else:
+            s = str(right) + ', ' + s
+        n -= pow_2
+    print(s + '}')
+    input_n = int(input())
