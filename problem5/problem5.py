7ebbe4a28545c02a81a2a547335ae206ff7789ae
Alen Sugimoto <alensugimoto@gmail.com>
2021-03-21 22:23:13 +0100

    correctly solve problem 5

--- problem5/problem5.py
+++ problem5/problem5.py
@@ -1,11 +1,14 @@
 max_shuffles = int(input())
+num_cards = 2 ** max_shuffles
 cards = input().split()
 num_shuffles = 0
 while num_shuffles < max_shuffles:
     num_cards_per_pile = 2 ** num_shuffles
-    while num_cards_per_pile > 1:
-        cards = cards[num_cards_per_pile // 2:num_cards_per_pile] + cards[3 * num_cards_per_pile // 2:] + cards[:num_cards_per_pile // 2] + cards[num_cards_per_pile:3 * num_cards_per_pile // 2]
-        num_cards_per_pile //= 2
-    cards[::2], cards[1::2] = cards[1::2], cards[::2]
+    new_cards = []
+    for i in range(0, num_cards, 2 * num_cards_per_pile):
+        for j in range(num_cards_per_pile - 1, -1, -1):
+            new_cards.append(cards[i + j + num_cards_per_pile])
+            new_cards.append(cards[i + j])
+    cards = new_cards
     num_shuffles += 1
 print(" ".join(cards))
