ff590f19c2cae97b3be2ba70095bdb69d356c941
Alen Sugimoto <alensugimoto@gmail.com>
2021-03-22 22:24:23 +0100

    correctly solve problem 4

--- problem4/problem4.py
+++ problem4/problem4.py
@@ -1,85 +1,31 @@
-def next_b(b):
-    new_b = str(int(b) + 1)
-    length = len(new_b)
-    zeros = 0
-    while int(new_b[-1 - zeros]) == 0:
-        zeros += 1
-    return str(int(b) - int(new_b[-1 - zeros] + "0" * zeros))
-
-
-def go_to_max(b, x):
-    if int(x) <= int(b[0]):
-        return x + "0" * (len(b) - 1)
-    else:
-        return b[0] + go_to_max(b[1:], str(int(x) - int(b[0])))
-
-
-def find_max(a, b, x):
-    if int(a) > int(b):
-        return "none"
-    else:
-        first = int(b[0])
-        length = len(b)
-        summ = sum([int(digit) for digit in b])
-        if summ == int(x):
-            return b
-        elif summ > int(x):
-            return go_to_max(b, x)
-        elif length == 1:
-            return "none"
-        else:
-            return find_max(a, next_b(b), x)
-
-
-def go_to_min(a, x):
-    summ = sum([int(digit) for digit in a])
-    nines = 0
-    add = 9 - int(a[-1])
-    while int(x) - summ > add:
-        if len(a) < nines + 1:
-            a = "9" + a
-        else:
-            a = a[:-1 - nines] + "9" + a[len(a) - nines:]
-        summ += add
-        nines += 1
-        if len(a) < nines + 1:
-            add = 9
-        else:
-            add = 9 - int(a[-1 - nines])
-    return str(int(a) + (int(x) - summ) * 10 ** nines)
-
-
-def find_min(a, b, x):
-    if int(a) >= int(b):
-        return b
-    else:
-        first = int(a[0])
-        length = len(a)
-        summ = sum([int(digit) for digit in a])
-        if summ == int(x):
-            return a
-        elif summ < int(x):
-            min = go_to_min(a, x)
-            if int(min) < int(b):
-                return min
-            else:
-                return b
-        else:
-            return find_min(str(10 ** length), b, x)
-
-
 num_questions = int(input())
 answers = []
 for _ in range(num_questions):
-    integers = input().split()
+    integers = [int(strint) for strint in input().split()]
     a = integers[0]
     b = integers[1]
     x = integers[2]
-    max = find_max(a, b, x)
-    if max == "none":
-        answers.append("none")
+    # way to ceil and floor without math module in the next two lines
+    # was taken from https://stackoverflow.com/a/32559082
+    min = x - 9 * ((x - a) // 9)
+    max = x + 9 * ((b - x) // 9)
+    min_found = False
+    i = min
+    while not min_found and i <= max:
+        if x == sum([int(d) for d in str(i)]):
+            min_found = True
+            min = i
+            max_found = False
+            i = max
+            while not max_found and i >= min:
+                if x == sum([int(d) for d in str(i)]):
+                    max_found = True
+                    max = i
+                i -= 9
+        i += 9
+    if min_found:
+        answers.append(str(min) + " " + str(max))
     else:
-        min = find_min(a, max, x)
-        answers.append(min + " " + max)
+        answers.append("none")
 for ans in answers:
     print(ans)
