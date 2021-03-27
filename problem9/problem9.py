59ab1f7c328979a2fd402c0e4ded0eba58f5bec3
Alen Sugimoto <alensugimoto@gmail.com>
2021-03-27 22:42:20 +0100

    try 2 of problem 9

--- problem9/problem9.py
+++ problem9/problem9.py
@@ -1,31 +1,25 @@
+def find_min(a, b, x):
+    zeros = 0
+    while True:
+        min = str(int(x) % 9) + zeros * "0" + (int(x) // 9) * "9"
+        zeros += 1
+        if int(min) > int(b):
+            return "none"
+        elif int(min) >= int(a):
+            return min
+
+
 num_questions = int(input())
-answers = []
 for _ in range(num_questions):
-    integers = [int(strint) for strint in input().split()]
-    a = integers[0]
-    b = integers[1]
-    x = integers[2]
-    # way to ceil and floor without math module in the next two lines
-    # was taken from https://stackoverflow.com/a/32559082
-    min = x - 9 * ((x - a) // 9)
-    max = x + 9 * ((b - x) // 9)
-    min_found = False
-    i = min
-    while not min_found and i <= max:
-        if x == sum([int(d) for d in str(i)]):
-            min_found = True
-            min = i
-            max_found = False
-            i = max
-            while not max_found and i >= min:
-                if x == sum([int(d) for d in str(i)]):
-                    max_found = True
-                    max = i
-                i -= 9
-        i += 9
-    if min_found:
-        answers.append(str(min) + " " + str(max))
-    else:
-        answers.append("none")
-for ans in answers:
-    print(ans)
+    a, b, x = [int(strint) for strint in input().split()]
+    min = find_min(a, b, x)
+    if min == "none":
+        print("none")
+        continue
+    min = int(min)
+    max = int(x) + 9 * ((int(b) - int(x)) // 9)
+    while max > min:
+        if int(x) == sum([int(d) for d in str(max)]):
+            break
+        max -= 9
+    print("%i %i" % (min, max))
