175951b91d88e3bd40d05133f926cff715fa6a5b
Alen Sugimoto <alensugimoto@gmail.com>
2021-04-06 14:18:04 +0200

    try 3 of problem 12

--- problem12/problem12.py
+++ problem12/problem12.py
@@ -1,10 +1,10 @@
 n, len_bases = [int(num) for num in input().split()]
-bases = [int(base) for base in input().split()]
+bases = input().split()
 num_queries = int(input())
 
 for _ in range(num_queries):
     l, r = [int(bound) for bound in input().split()]
     count = 0
     for i in range(len_bases):
-        count += r // bases[i] - (l - 1) // bases[i]
+        count += r // int(bases[i]) - (l - 1) // int(bases[i])
     print(count)
